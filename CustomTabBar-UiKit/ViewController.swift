b420e71a106b24fd594f762eee4126547736a63d
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-23 13:22:11 +0500

    Initial Commit

---
+++ CustomTabBar-UiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabBar-UiKit
+//
+//  Created by Zaid Sheikh on 23/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
